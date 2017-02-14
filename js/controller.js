bb44e7decbc4f9a7ab72bd1723fd0bdcc1cacc2d
Maisie Campbell <maisie-campbell@hotmail.com>
2017-02-14 12:13:51 +0000

    fixed issue with link fragments and commented out unimplemented functions to get console feedback

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,22 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Angular Todo!"
+        $scope.title = "Welcome To Angular Todo!";
     })
-    .controller('RegisterController', function($scope, UserAPIService) {
+    .controller('RegisterController', function($scope, UserAPIService, store) {
         $scope.registrationUser = {};
         var URL = "https://morning-castle-91468.herokuapp.com/";
+
 
         $scope.login = function() {
             UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
                 $scope.token = results.data.token;
+                store.set('username', $scope.registrationUser.username);
+                store.set('authToken', $scope.token);
                 console.log($scope.token);
             }).catch(function(err) {
                 console.log(err.data);
             });
-        }
+        };
 
         $scope.submitForm = function() {
             if ($scope.registrationForm.$valid) {
