5bf5357b383c381956a8e32984d00cf540ef4cbd
Maisie Campbell <maisie-campbell@hotmail.com>
2017-02-20 12:04:43 +0000

    created editTodo and deleteTodo methons in service.js and in controller

--- js/controller.js
+++ js/controller.js
@@ -85,7 +85,21 @@ angular.module('RouteControllers', [])
                 console.log(err);
         });
     };
+})
+
+.controller('EditTodoController', function($scope, $location, $routeParams, TodoApiService, store){
+    var id = $routeParams.id;
+    var URL ="https://morning-castle-91468.herokuapp.com/";
+
+    $scope.submitForm = function(){
+        if($scope.todoForm.$valid){
+            $scope.todo.username = $scope.username;
+
+            TodoAPIService.editTodo(URL + "todo/" + id, $scope.todo, store.get('authToken')).then(function(results){
+                $location.path("/todo");
+            }).catch(function(err) {
+                console.log(err);
+           });
+        }
+    };
 });
-    
-     
-    
