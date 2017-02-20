e8db7e3d352519a9cdc3bbd29a85bc2af1c35fe2
Maisie Campbell <maisie-campbell@hotmail.com>
2017-02-20 23:17:19 +0000

    directives lesson in angular js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage']);
+angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', 'TodoService', 'TodoDirective']);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
+	
 	$routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeController'
-    })
-    .when('/accounts/register', {
-        templateUrl: 'templates/register.html',
-        controller: 'RegisterController'
-    })
-    .when('/todo', {
-        templateUrl: 'templates/todo.html',
-        controller: 'TodoController'
-    });
-
-  $locationProvider.html5Mode(true);
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+	.when('/accounts/register', {
+		templateUrl: 'templates/register.html',
+		controller: 'RegisterController'
+	})
+	.when('/todo', {
+		templateUrl: 'templates/todo.html',
+		controller: 'TodoController'
+	}).when('/todo/edit/:id', {
+	    templateUrl:'templates/edit-todo.html',
+	    controller: 'EditTodoController'
+	});
+	  $locationProvider.html5Mode(true);
 });
+
+ 
+
